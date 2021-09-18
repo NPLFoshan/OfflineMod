e87b4474d058fec38156803ad1a98939705a1aec
onedou <onedou@126.com>
2021-09-18 18:21:57 +0800

    add user mac binds api and service

---
+++ Mod/OfflineMod/service/UserMacBindsService.lua
@@ -0,0 +1,10 @@
+--[[
+Title: Account Manager
+Author(s):  Big
+Date: 2021.09.08
+Desc: 
+use the lib:
+------------------------------------------------------------
+local AccountManager = NPL.load('(gl)Mod/Offline/AccountManager.lua')
+------------------------------------------------------------
+]]
